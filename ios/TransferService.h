6dd22765993d866a61dc9f6c715d2dceb4b6e526
Ömer Akyol <omer@tulparyazilim.com.tr>
2022-07-20 18:30:47 +0300

    UUID changed

--- ios/TransferService.h
+++ ios/TransferService.h
@@ -1,7 +1,7 @@
 #ifndef LE_Transfer_TransferService_h
 #define LE_Transfer_TransferService_h
 
-#define TRANSFER_SERVICE_UUID           @"fd05a570-274a-4b1f-a5a3-eb52e5e02b8b"
-#define TRANSFER_CHARACTERISTIC_UUID    @"53447ca9-1e5b-448e-ab7b-bd9438c048af"
+#define TRANSFER_SERVICE_UUID           @"18eba6c4-4876-4920-83d4-1839005c9724"
+#define TRANSFER_CHARACTERISTIC_UUID    @"52d2cdd6-7204-4ad7-bb58-93b878c33f87"
 
 #endif
